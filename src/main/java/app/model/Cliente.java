764650035dd167dac23d11de7ff5a4d3e370c538
gabryeldev <gabryel.araujodev@gmail.com>
2022-09-27 14:38:21 -0300

    Alteração na classe Cliente

--- src/main/java/app/model/Cliente.java
+++ src/main/java/app/model/Cliente.java
@@ -1,10 +1,15 @@
 package app.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -24,10 +29,15 @@ public class Cliente {
 	@Size(max = 100, message = "O Nome deve conter no máximo 100 caracter!")
 	private String nome;
 	
-	@NotBlank(message = "O E-mail não pode ser vazio!")
-	@Column(name = "EMAIL")
-	@Size(max = 100, message = "O E-mail deve conter no máximo 100 caracter!")
-	private String email;
+	@NotBlank(message = "O Sobrenome não pode ser vazio!")
+	@Column(name = "SOBRENOME")
+	@Size(max = 100, message = "O Sobrenome deve conter no máximo 100 caracter!")
+	private String sobrenome;
+	
+	@NotBlank(message = "A Senha não pode ser vazia!")
+	@Column(name = "SENHA")
+	@Size(min = 6, message = "A Senha deve conter no mínimo 6 caracter!")
+	private String senha;
 	
 	@Column(name = "TELEFONE")
 	private String telefone;
@@ -37,9 +47,18 @@ public class Cliente {
 	@Size(min = 11, max = 11, message = "O CPF deve conter 11 digitos!")
 	private String cpf;
 	
+	@NotBlank(message = "O E-mail não pode ser vazio!")
+	@Column(name = "EMAIL")
+	@Size(max = 100, message = "O E-mail deve conter no máximo 100 caracter!")
+	private String email;
+	
 	@NotBlank(message = "O RG não pode ser vazio!")
 	@Column(name = "RG", unique = true)
 	private String rg;
+	
+	
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.PERSIST)
+	private List<Endereco> endereco;
 
 	public Long getId() {
 		return id;
@@ -57,12 +76,20 @@ public class Cliente {
 		this.nome = nome;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getSobrenome() {
+		return sobrenome;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public String getTelefone() {
@@ -81,6 +108,14 @@ public class Cliente {
 		this.cpf = cpf;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getRg() {
 		return rg;
 	}
@@ -88,7 +123,12 @@ public class Cliente {
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
-	
-	
-	
+
+	public List<Endereco> getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(List<Endereco> endereco) {
+		this.endereco = endereco;
+	}	
 }
---
+++ src/main/java/app/model/Endereco.java
@@ -0,0 +1,34 @@
+package app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Endereco {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToOne
+	private Cliente cliente;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+}
