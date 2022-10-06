6914c7dd9df3eaea5ab297d59bcfbb2ee621e664
gabryeldev <gabryel.araujodev@gmail.com>
2022-10-06 13:34:33 -0300

    Implementação da validação de login

--- src/main/java/app/repository/ClienteRepository.java
+++ src/main/java/app/repository/ClienteRepository.java
@@ -3,10 +3,18 @@ package app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.model.Cliente;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 	boolean existsByCpf(String cpf);
 
 	boolean existsByRg(String rg);
+
+	boolean existsByEmail(String email);
+
+	@Query(value = "SELECT senha FROM TB_CLIENTE WHERE email = email", nativeQuery = true)
+	String getSenhaByEmail(String email);
 }
--- target/generated-sources/annotations/app/mapper/ClienteMapperImpl.java
+++ target/generated-sources/annotations/app/mapper/ClienteMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-05T14:50:01-0300",
+    date = "2022-10-06T11:53:51-0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (IBM Corporation)"
 )
 @Component
