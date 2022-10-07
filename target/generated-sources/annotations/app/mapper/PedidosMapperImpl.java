907d731ff1ad8c0826718104c428523b077209a3
otavio.SilvaN <otavio3397@gmail.com>
2022-10-07 16:18:37 -0300

    Alterações na classe ItensPedidos

---
+++ target/generated-sources/annotations/app/mapper/PedidosMapperImpl.java
@@ -0,0 +1,42 @@
+package app.mapper;
+
+import app.dto.pedidosDto.PedidosRequestDTO;
+import app.dto.pedidosDto.PedidosResponseDTO;
+import app.model.Pedidos;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-10-07T15:56:04-0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (IBM Corporation)"
+)
+@Component
+public class PedidosMapperImpl implements PedidosMapper {
+
+    @Override
+    public Pedidos toPedidos(PedidosRequestDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Pedidos pedidos = new Pedidos();
+
+        return pedidos;
+    }
+
+    @Override
+    public PedidosResponseDTO toPedidosResponseDTO(Pedidos ped) {
+        if ( ped == null ) {
+            return null;
+        }
+
+        PedidosResponseDTO pedidosResponseDTO = new PedidosResponseDTO();
+
+        pedidosResponseDTO.setId( ped.getId() );
+        pedidosResponseDTO.setValorTotal( ped.getValorTotal() );
+        pedidosResponseDTO.setStatus( ped.getStatus() );
+
+        return pedidosResponseDTO;
+    }
+}
