29dd2521fe61e0d0ccbe5e4c676ea5fc41a79717
otavio.SilvaN <otavio3397@gmail.com>
2022-10-10 10:50:30 -0300

    Implementacao categoria.mapper e categoriaDTO

--- src/main/java/app/dto/categoriaDto/CategoriaRequestDTO.java
+++ src/main/java/app/dto/categoriaDto/CategoriaRequestDTO.java
@@ -1,4 +1,21 @@
 package app.dto.categoriaDto;
 
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+
 public class CategoriaRequestDTO {
+
+    @Column(name="nome")
+    @NotBlank(message = "O campo Nome não pode ser nulo ou vazio")
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 }
+
+
